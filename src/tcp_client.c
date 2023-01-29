3a43b481fb1704b86fb6be04a77d55d57c50bdf8
Matthew Teta <matthewtetadev@gmail.com>
2023-01-29 01:47:08 -0700

    Added the ls subprocess but there is a segfault occuring

--- src/tcp_client.c
+++ src/tcp_client.c
@@ -17,11 +17,33 @@ void printv(char *msg) {
 }
 
 /**
- *   @brief Wrapper around perror, exit with {code}
+ * Wrapper around perror, exit with {code}
  */
 void error(char *msg, int code) {
   perror(msg);
   exit(code);
+}
+
+/**
+ * convert a stdin string and split on the first space,
+ * also replace the final newline character with a \0
+ * for use with globbing
+ */
+int processCmdString(char *cmd, char **opcode, char **arg2) {
+  if (!cmd || !opcode || !arg2)
+    return -1;
+  *opcode = cmd;
+  *arg2   = NULL;
+  while (*cmd != '\0') {
+    if (!*arg2 && *cmd == ' ') {
+      *cmd  = '\0';
+      *arg2 = cmd + 1;
+    }
+    if (*cmd == '\n')
+      *cmd = '\0';
+    cmd++;
+  }
+  return 0;
 }
 
 void printUsage() {
@@ -48,38 +70,46 @@ int main(int argc, char **argv) {
   ssize_t len;
   //   wordexp_t arglist;
   char  *opcode = NULL;
+  char  *arg2   = NULL;
   glob_t paths;
+  char path[4096];
+
+  // Loop forever accepting commands typed into stdin
   for (;;) {
-    int cmd_argc = 1;
     printf("> ");
     if ((len = getline(&cmd, &buflen, stdin)) <= 0)
       continue;
     // Parse command
-    size_t s = strlen(cmd);
-    if (1 != sscanf(cmd, "%m[a-z]", &opcode)) {
-      printf("Invalid command.\n");
-      continue;
-    }
+    // size_t s = strlen(cmd);
+    // if (1 != sscanf(cmd, "%m[a-z]", &opcode)) {
+    //   printf("Invalid command.\n");
+    //   continue;
+    // }
     // Update command pointer to read the second argument (a little jenky but
     // ok)
-    if (s > strlen(opcode)) {
-      printv("TEST");
-      cmd_argc = 2;
-      cmd += strlen(opcode) + 1;
-      // Remove the trailing newline for globbing
-      s = strlen(cmd);
-      if (s && (cmd[s - 1] == '\n'))
-        cmd[--s] = 0;
-    }
+    // if (s > strlen(opcode)) {
+    //   printv("TEST");
+    //   cmd_argc = 2;
+    //   cmd += strlen(opcode) + 1;
+    //   // Remove the trailing newline for globbing
+    //   s = strlen(cmd);
+    //   if (s && (cmd[s - 1] == '\n'))
+    //     cmd[--s] = 0;
+    // }
+
+    processCmdString(cmd, &opcode, &arg2);
 
     // Parse based on opcode
     if (0 == strcmp("get", opcode)) {
       printv("GET");
-      if (cmd_argc != 2) {
+      if (arg2 == NULL) {
         puts("Must provide a path argument to 'get' command.");
         continue;
       }
-      if (0 != glob(cmd, GLOB_MARK, NULL, &paths)) {
+      // printf("arg2: %s\n", arg2);
+      // for (char *c = arg2; *c; c++)
+      //   printf("%2X\n", *c);
+      if (0 != glob(arg2, GLOB_MARK, NULL, &paths)) {
         printf("Error matching filepath.\n");
         continue;
       }
@@ -89,18 +119,27 @@ int main(int argc, char **argv) {
       }
     } else if (0 == strcmp("put", opcode)) {
       printv("PUT");
-      if (cmd_argc != 2) {
+      if (arg2 == NULL) {
         puts("Must provide a path argument to 'put' command.");
         continue;
       }
     } else if (0 == strcmp("delete", opcode)) {
       printv("DELETE");
-      if (cmd_argc != 2) {
+      if (arg2 == NULL) {
         puts("Must provide a path argument to 'delete' command.");
         continue;
       }
     } else if (0 == strcmp("ls", opcode)) {
       printv("LS");
+      FILE *fp = popen("/bin/ls", "r");
+      if (!fp)
+        error("Failed to run the ls command. Is it in your path?", -2);
+      while (fgets(path, sizeof(path), fp) != NULL) {
+        printf("%s", path);
+      }
+      // printf("%s", )
+      if (-1 == pclose(fp))
+        error("PCLOSE ERROR", -2);
     } else if (0 == strcmp("exit", opcode)) {
       printv("EXIT");
       exit(0);
@@ -110,7 +149,6 @@ int main(int argc, char **argv) {
     }
 
     globfree(&paths);
-    free(opcode);
   }
 
   return 0;
